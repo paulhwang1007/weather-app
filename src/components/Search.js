19996501440e04a9639d3d5f482c839ea9cc698f
Paul Hwang <hwpaul1007@gmail.com>
2024-12-27 21:26:48 -0800

    Implement Search

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import '../styles/components/Search.scss'
 
 function Search() {
   return (
-    <div>
-      Search
+    <div className='search-container'>
+      <div className='search-icon'>
+        <i className='bi bi-search'></i>
+      </div>
+      <div className='search-input'>
+        <input 
+        type='text'
+        name='search-city'
+        placeholder='Search city ...'
+        />
+      </div>
     </div>
   )
 }
