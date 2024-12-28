ef216138f7063541071f12cd48c86b70cc3eb27a
Paul Hwang <hwpaul1007@gmail.com>
2024-12-27 21:23:28 -0800

    Install bootstrap icons and implement Place CSS

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import Main from "./components/Main";
 import './styles/components/App.scss'
+import 'bootstrap-icons/font/bootstrap-icons.css'
 
 function App() {
   const dark = false;
--- src/components/Place.js
+++ src/components/Place.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Place() {
   return (
     <div className='Place'>
-      Place
+      <i class="bi bi-geo-alt-fill"></i> <strong>London</strong>, United Kingdom
     </div>
   )
 }
