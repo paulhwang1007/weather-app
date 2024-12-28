9b3518ef2c7ee350972587e29625b0f4310c33c0
Paul Hwang <hwpaul1007@gmail.com>
2024-12-27 21:30:21 -0800

    Implement Settings: Light & Dark Theme btns

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import '../styles/components/Settings.scss'
 
 function Settings() {
   return (
-    <div>
-      Settings
+    <div className='Settings'>
+      <div className='theme-toggler'>
+        <div className='theme-buttons'>
+          <div className='light-theme-btn active'>
+            <i className='bi bi-sun'></i>
+          </div>
+          <div className='dark-theme-btn'>
+            <i className='bi bi-moon'></i>
+          </div>
+        </div>
+      </div>
+      <div className='settings-btn'>
+        <i className='bi bi-gear'></i>
+      </div>
     </div>
   )
 }
