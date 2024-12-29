4ceb2f8335390a8dd3bb6a556b46ee77ecf53ccf
Paul Hwang <hwpaul1007@gmail.com>
2024-12-28 22:18:16 -0800

    Make final changes
    Make icons not draggable, etc

--- src/components/HourlyForecastWidget.js
+++ src/components/HourlyForecastWidget.js
@@ -7,34 +7,40 @@ function HourlyForecastWidget({ data }) {
   const { date, icon, summary, temperature, precipitation, wind } = data;
 
   // date format
+  const locale = navigator.language;
   const now_date = {
-    day: new Intl.DateTimeFormat(navigator.language, {
+    day: new Intl.DateTimeFormat(locale, {
       weekday: "short",
       day: "2-digit",
       month: "2-digit",
     }).format(new Date()),
-    time: new Intl.DateTimeFormat(navigator.language, {
+    time: new Intl.DateTimeFormat(locale, {
       hour: "2-digit",
       minute: "2-digit",
     }).format(new Date().setMinutes(0)),
   };
 
   const weather_date = {
-    day: new Intl.DateTimeFormat(navigator.language, {
+    day: new Intl.DateTimeFormat(locale, {
       weekday: "short",
       day: "2-digit",
       month: "2-digit",
     }).format(new Date(date)),
-    time: new Intl.DateTimeFormat(navigator.language, {
+    time: new Intl.DateTimeFormat(locale, {
       hour: "2-digit",
       minute: "2-digit",
     }).format(new Date(date).setMinutes(0)),
   };
 
+  const midnightTime = new Intl.DateTimeFormat(locale, {
+    hour: "2-digit",
+    minute: "2-digit",
+  }).format(new Date().setHours(0, 0, 0, 0));
+
   weather_date.day =
     weather_date.day === now_date.day && weather_date.time === now_date.time
-      ? "Today"
-      : weather_date.time === "12:00 AM"
+      ? "Now"
+      : weather_date.time === midnightTime
       ? weather_date.day
       : "";
 
--- src/components/WeatherIcon.js
+++ src/components/WeatherIcon.js
@@ -5,6 +5,7 @@ function WeatherIcon({ iconNumber, summary }) {
     <img
       src={`${process.env.PUBLIC_URL}/dist/weather_icons/set04/big/${iconNumber}.png`}
       alt={summary}
+      draggable={false}
     />
   );
 }
